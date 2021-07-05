5729f9e9a21e7aba9dfe052e4bb11d74700fb0df
YirimS <yirimseck@gmail.com>
2021-07-05 08:59:24 -0700

    refactored arrow function and checked console

--- prework/arrow-functions/starter-code/app.js
+++ prework/arrow-functions/starter-code/app.js
@@ -174,12 +174,12 @@ console.log(joe.greeting());
 
 
 
-Student.courseName = function() {
+Student.courseName = () => {
   return 'This student is enrolled in Code 301.';
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
