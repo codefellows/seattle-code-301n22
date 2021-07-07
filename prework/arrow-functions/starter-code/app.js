b9645de878cfec34555c3b4480f6f8a7387cec2e
DrPayne25 <alex.payne1125@gmail.com>
2021-07-07 13:38:32 -0700

    Step 11

--- prework/arrow-functions/starter-code/app.js
+++ prework/arrow-functions/starter-code/app.js
@@ -216,18 +216,18 @@ Student.prototype.scope = function() {
   console.log(this);
 };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// When joe.scope is invoked we are looking at an instance of the Joe Student object
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// When joe.scopeArrow is invoked we are looking at the instance of the browsers I am assuming
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// It is differnt because arrow functions can't be used with this when writing code.
